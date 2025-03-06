24cc16f3d7149e3a8f416755664727c572ea176c
vaibhavgoswami87 <vaibhavgoswami055@gmail.com>
2025-03-06 22:19:13 +0530

    added

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,12 +1,48 @@
 import React from 'react';
 
 
+let ImageUrl = "https://source.unsplash.com/random/900x700/?fruit";
+
 // Landing Page Component
 const LandingPage = () => {
   return (
-    <div>
-      this is landing page
-    </div>
+    <section className="sec-1  d-flex flex-column flex-lg-row align-items-center justify-content-center">
+      <div className=" px-5 py-4">
+        <button className="btn-head px-2 py-1 rounded">
+          😍 Award-winning digital services
+        </button>
+        <h1 className="display-4 fw-bold">
+          Creative studio<div> focused on</div>
+          <div>Web- Devloper</div>
+        </h1>
+        <p className="w-75">
+          From concept to execution, we deliver web designs that make an
+          impact. Let your brand shine with our innovative and visually
+          stunning websites.
+        </p>
+        <div className="d-flex gap-2 ">
+          <div className="mt-3">
+            <input
+              type="text"
+              className="rounded px-2 w-auto border border-secondary border-2 "
+              placeholder="example@email.com"
+            />
+            <button className=" text-light btn w-md-auto bg-black py-2">
+              Start Trial!
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className=" w-50 d-flex align-items-center position-relative">
+        <img
+          src={ImageUrl}
+          alt=""
+          style={{ width: "25rem" }}
+          className="img-fluid"
+        />
+        <div className="position-absoulte" />
+      </div>
+    </section>
   );
 };
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,185 @@
 import React from "react";
 
+let ImageUrl = "https://source.unsplash.com/random/900x700/?fruit";
+
+
 // Landing Page Component
 const navbar = () => {
-  return <div>this is navbar</div>;
+  return (<nav className="navbar navbar-expand-lg bg-body-tertiary d-flex justify-content-around">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#f">
+        <img
+          src={ImageUrl}
+          alt="Logo"
+          width={100}
+          height={24}
+          className="d-inline-block align-text-top"
+        />
+      </a>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mx-auto mb-2  mb-lg-0">
+          <li className="nav-item dropdown">
+            <a
+              className="nav-link dropdown-toggle"
+              href="#f"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Demos
+            </a>
+            <ul className="dropdown-menu">
+              <li>
+                <a className="dropdown-item" href="#f">
+                  Action
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#f">
+                  Another action
+                </a>
+              </li>
+              <li>
+                <hr className="dropdown-divider" />
+              </li>
+              <li>
+                <a className="dropdown-item" href="#f">
+                  Something else here
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li className="nav-item dropdown">
+            <a
+              className="nav-link dropdown-toggle"
+              href="#f"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Pages
+            </a>
+            <ul className="dropdown-menu">
+              <li>
+                <a className="dropdown-item" href="#f">
+                  Action
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#f">
+                  Another action
+                </a>
+              </li>
+              <li>
+                <hr className="dropdown-divider" />
+              </li>
+              <li>
+                <a className="dropdown-item" href="#f">
+                  Something else here
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li className="nav-item dropdown">
+            <a
+              className="nav-link dropdown-toggle"
+              href="#f"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Portfolio
+            </a>
+            <ul className="dropdown-menu">
+              <li>
+                <a className="dropdown-item" href="#f">
+                  Action
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#f">
+                  Another action
+                </a>
+              </li>
+              <li>
+                <hr className="dropdown-divider" />
+              </li>
+              <li>
+                <a className="dropdown-item" href="#f">
+                  Something else here
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li className="nav-item dropdown">
+            <a
+              className="nav-link dropdown-toggle"
+              href="#f"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Magamenu
+            </a>
+            <ul className="dropdown-menu">
+              <li>
+                <a className="dropdown-item" href="#f">
+                  Action
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#f">
+                  Another action
+                </a>
+              </li>
+              <li>
+                <hr className="dropdown-divider" />
+              </li>
+              <li>
+                <a className="dropdown-item" href="#f">
+                  Something else here
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#f">
+              Contact Us
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="d-flex align-items-center gap-2">
+        <div className="d-flex gap-2">
+          <i className="bi bi-brightness-low-fill fs-5" />
+          <button
+            className="rounded px-2 btn-outline-primary btn-sm d-none d-sm-block"
+            type="submit"
+          >
+            <i className="bi bi-person-circle me-2" />
+            Sign up
+          </button>
+          <button
+            className="btn btn-primary btn-sm d-none d-sm-block"
+            type="submit"
+          >
+            Buy now
+          </button>
+        </div>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+      </div>
+    </div>
+  </nav>);
 };
 
 export default navbar;
